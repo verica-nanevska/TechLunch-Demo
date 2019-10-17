9e2b01810e9229b1155a74ee2303789854143894
Pierre Maris <pierre.maris@capgemini.com>
2019-10-17 10:27:25 +0200

    set "books" at controller level

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -1,19 +1,21 @@
 package com.example.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @RestController
+@RequestMapping("/books")
 public class TestController {
 	
 	private static List<String> books = new ArrayList<>();
@@ -24,34 +26,33 @@ public class TestController {
 	}
 	
 	@ResponseStatus(HttpStatus.OK)
-	@GetMapping("/books")
+	@GetMapping("/")
 	public List<String> getData() {
 		return books;
 	}
 	
 	@ResponseStatus(HttpStatus.OK)
-	@GetMapping("/books/{id}")
+	@GetMapping("/{id}")
 	public String getBook(@PathVariable("id") int index) {
 		return books.get(index);
 	}
 	
 	@ResponseStatus(HttpStatus.CREATED)
-	@PostMapping("/books")
+	@PostMapping("/")
 	public int createBook(@RequestParam String title) {
 		books.add(title);
 		return books.size()-1;
 	}
 	
 	@ResponseStatus(HttpStatus.OK)
-	@PutMapping("/books/{id}")
+	@PutMapping("/{id}")
 	public String updateBook(@PathVariable("id") int index, @RequestParam String title) {
-		
 		books.set(index, title);
 		return books.get(index);
 	}
 	
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	@DeleteMapping("/books/{id}")
+	@DeleteMapping("/{id}")
 	public void deleteBook(@PathVariable("id") int index) {
 		books.remove(index);
 	}
